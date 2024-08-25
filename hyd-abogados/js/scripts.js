984a984c84e946cf3a7c57103021e3ea2f8b9782
Eduardo Mora <72584241+emorar@users.noreply.github.com>
2024-08-24 23:54:52 -0400

    Add files via upload

---
+++ hyd-abogados/js/scripts.js
@@ -0,0 +1,23 @@
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('main-header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        const offsetPosition = targetElement.offsetTop - 150; /* Ajusta según la altura de tu header */
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+});
